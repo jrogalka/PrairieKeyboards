73e42e7421230d032a037214a8053c05e76aade5
Joel Rogalka <jrogalka@outlook.com>
2020-11-25 12:33:31 -0600

    Adds products controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+require "faker"
+require "json"
+
+# AdminUser.delete_all
+Product.delete_all
+
+# USED TO GENERATE 10 INITIAL SAFE PRODUCTS
+
+file = File.read("./public/data.json")
+data = JSON.parse(file)
+i = 0
+
+10.times do
+  product = Product.new
+  product.name = data[i]["name"]
+  product.sku = data[i]["sku"]
+  product.description = data[i]["description"]
+  product.price = data[i]["price"]
+  product.is_assembled = data[i]["is_assembled"]
+  product.category = Category.find(data[i]["category_id"])
+
+  if product&.valid?
+    puts "Created #{product.name}"
+    product.save
+  else
+    product.errors.each { |attr, msg| puts "#{attr} - #{msg}\n" }
+  end
+
+  i += 1
+end
+
+# Generate 100 temporary products
+100.times do
+  category = Category.find(rand(1..4))
+
+  product = category.products.create(
+    name:         Faker::Computer.stack + Faker::Computer.type,
+    sku:          Faker::Number.number(digits: 10),
+    description:  Faker::Coffee.notes,
+    price:        Faker::Number.between(from: 100, to: 3000),
+    is_assembled: Faker::Boolean.boolean
+  )
+  if product&.valid?
+    puts "Created #{product.name}"
+    product.save
+  else
+    product.errors.each { |attr, msg| puts "#{attr} - #{msg}\n" }
+  end
+end
+
+# if Rails.env.development?
+#   AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
+# end
