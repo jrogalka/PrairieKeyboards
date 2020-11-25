0c5708387a22b6e4343c400107dc42427f60aee3
Joel Rogalka <jrogalka@outlook.com>
2020-11-25 12:35:47 -0600

    Adds intial Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  resources :products, only: %i[index show]
+  root to: "products#index"
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
