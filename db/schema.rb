929d7d0c061a8ee94806ae25fa77d5d718be9e1d
Joel Rogalka <jrogalka@outlook.com>
2020-11-25 12:17:59 -0600

    Adds Categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_181629) do
+ActiveRecord::Schema.define(version: 2020_11_25_181743) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "full_name"
