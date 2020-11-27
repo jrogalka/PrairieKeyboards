c66479ba151f0501a03766784ee1ecde07ec0f90
Joel Rogalka <jrogalka@outlook.com>
2020-11-26 21:33:16 -0600

    Adds Customer creation and authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password, :province_id, :shipping_address) }
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password, :province_id, :shipping_address) }
+  end
 end
