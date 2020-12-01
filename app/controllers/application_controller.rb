f99df3a1b2f1df6184005f72378e4b1500c1fc4d
Joel Rogalka <jrogalka@outlook.com>
2020-12-01 11:39:29 -0600

    Adds shopping cart methods and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :initialize_session
+  helper_method :cart
 
   protected
 
@@ -7,5 +9,17 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password, :province_id, :shipping_address) }
 
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password, :province_id, :shipping_address) }
+  end
+
+  private
+
+  def initialize_session
+    # Initialize shopping cart
+    session[:shopping_cart] ||= {}
+  end
+
+  def cart
+    # Return a collection of product objects based on the products within the shopping cart
+    Product.find(session[:shopping_cart])
   end
 end
---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,14 @@
+class CartController < ApplicationController
+  # POST /cart
+  # Data sent as form data, ends up in the params hash
+  def create
+    # Add params[:id] to cart
+    i = 0
+  end
+
+  # DELETE /cart/:id
+  def destroy
+    # Remove params[:id] from cart
+    i = 0
+  end
+end
